a847366e97e3027288e90bb3cd2a248b453a98e7
anshulgera <anshulgera351gmail.com>
2020-12-03 19:58:51 +0530

    updated command

--- basic_commands.sh
+++ basic_commands.sh
@@ -21,8 +21,12 @@ cat  # concatenate files and print on the standard output
 echo # display a line of text
 grep # print lines matching a pattern
 # examples of grep : grep -r 'string', egrep, zgrep -e
+strings # strings is used to extract all printable character strings found in the file or files given as arguments. It is useful in locating human-readable content embedded in binary files; for text files one can just use grep.
+tr # translate or delete characters , cat city | tr [a-z] [A-Z], tr '{}' '[]' inputfile outputfile, 
+tee # tee takes the output from any command, and, while sending it to standard output, it also saves it to a file. find -name "g*cfs" | tee /tmp/tee_output
 wc # print the number of newlines words and bytes in files
-sort #sort lines of text file
+sort #sort lines of text file, sort filename, sort -r filename, sort -k 3 filename, sort -u filename  
+uniq # uniq removes duplicate consecutive lines in a text file and is useful for simplifying the text display. sort file1 file2 | uniq > file3, uniq -c filename
 chmod # change file access permission
 chown # change file owner and group
 su  # change user id or become super user
@@ -39,8 +43,10 @@ fdisk # partition manipulator
 tail file_name # print last 10 lines of a file.
 free # to check the RAM of a server
 nproc #number of CPUs in a server.
-cut #
-paste #
+cut # cut is used for manipulating column-based files and is designed to extract specific columns. ls -l | cut -d" " -f3
+paste # combine fields(such as name or phone number from different files) $ paste file1 file2, $ paste -d file1 file2
+join # combine fields if similar columns are their in files $ join file1 file2
+split # split file in 100 equal-sized segments, $ split filename 
 sed # used for modifying the files
 awk # find and replaces text
 diff # identify the deferences between 2 files
@@ -61,3 +67,19 @@ iptraf # Monitors network traffic in text mode
 mtr #Combines functionality of ping and traceroute and gives a continuously updated display
 lynx, elinks, w3m # NON graphical browsers for linux  
 ftp, sftp, ncftp, yafc(yet another FTP client) # FTP clients enable you to transfer files with remote computers using the FTP protocol
+zcat compressed-file.txt.gz	# To view a compressed file
+zless somefile.gz #	To page through a compressed file
+or
+zmore somefile.gz #	To page through a compressed file
+zgrep -i less somefile.gz # To search inside a compressed file
+zdiff file1.txt.gz file2.txt.gz #	To compare two compressed files
+useradd # $ sudo useradd -m -c "Anshul gera" -s /bin/bash anshul
+#   $ sudo passwd anshul
+userdel # $ sudo userdel -r anshul
+groupadd #
+groupdel #
+env #
+set #
+export #
+echo PS1 # prints how you are promting the name of host and current working directory \u Username, \h hostname, \w current working dir, \! history number of this command, \d date
+# History environment variables like HISTFILE, HISTFILESIZE, HISTSIZE, HISTCONTROL, HISTIGNORE
