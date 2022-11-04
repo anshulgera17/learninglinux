9decd889b5db5487696d5bdc3909b5f3f46bccae
Anshul Gera <42892142+anshulgera17@users.noreply.github.com>
2022-11-04 16:19:51 +0530

    Update testcase.sh
    updated case script

--- testcase.sh
+++ testcase.sh
@@ -14,4 +14,20 @@ case "$response" in
 	"n" | "N" | "NO" ) echo "You have some common sense! Aborting...";;
 	*  ) echo "You have to give an answer!" ;;
 esac
+
+echo -e "Enter some character : \c"
+read value
+
+case $value in 
+	[a-z] ) 
+	echo "user entered $value a to z" ;;
+	[A-Z] ) 
+	echo "user entered $value A to Z" ;;
+	[0-9] ) 
+	echo "user entered $value 0 to 9" ;;
+	? )  
+	echo "user entered $value special character" ;;
+	* )
+	echo "unknown input" ;;
+esac
 exit 0
